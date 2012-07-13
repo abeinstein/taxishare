7679d54d6e6d4c50b03455bb99095caba161a699
Andrew <abeinstein@uchicago.edu>
2012-07-13 12:46:41 -0500

    Added choices

--- taxis/models.py
+++ taxis/models.py
@@ -12,8 +12,23 @@ class Passenger(models.Model):
         return self.firstName + " " + self.lastName
 
 class Taxi(models.Model):
-    startLoc = models.CharField(max_length=100)
-    endLoc = models.CharField(max_length=100)
+    START_LOC_CHOICES = (
+        ('South': 'South Campus (SCRH)'),
+        ('Reg': "Regenstein Library (57th b/w Ellis and Uni)"),
+        ('Ratner': "Ratner Athletic Center (55th and Ellis)"),
+        ('Stony': "Stony Island (57th and Stony)"),
+        ('Ida': "Ida Noyes (59th and Woodlawn)"),
+        ('Kim': "Kimbark Plaza (53rd and Woodlawn)")
+    )
+    
+    END_LOC_CHOICES = (
+        ('Oha': "O'Hare International Airport"),
+        ('Mid': "Midway International Airport"),
+        ('Uni': "Union Station")
+    )
+    
+    startLoc = models.CharField(max_length=100, choices=START_LOC_CHOICES)
+    endLoc = models.CharField(max_length=100, choices=END_LOC_CHOICES)
     startTime = models.DateTimeField()
     passengers = models.ManyToManyField(Passenger)
     
